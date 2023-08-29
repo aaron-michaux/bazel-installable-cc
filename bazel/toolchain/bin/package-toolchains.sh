6f28ea0611d8b832ee811f10afb97be16f5d0db3
Aaron Michaux <aaron@pageofswords.net>
2023-08-29 14:33:13 -0400

    saner package script

--- bazel/toolchain/bin/package-toolchains.sh
+++ bazel/toolchain/bin/package-toolchains.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 
+set -e
+
 TOOLCHAINS_DIR="/opt/toolchains"
+THIS_DIR="$(cd $(dirname "$0") ; pwd)"
+LLVMS="llvm-16.0.6 llvm-15.0.7 llvm-14.0.6 llvm-13.0.1"
+GCCS="gcc-13.2.0 gcc-12.3.0 gcc-11.4.0 gcc-10.5.0 gcc-9.5.0"
+
+show_help()
+{
+    cat <<EOF
+
+   Usage: $(basename $0) [build|archive] [<toolchain>|:all]
+
+      Where toolchain can be one of:
+
+         $LLVMS
+         $GCCS
+    
+EOF
+}
 
 VENDOR=""
 if [ -x "/usr/bin/lsb_release" ] ; then    
@@ -16,7 +35,7 @@ elif [ "$(uname -s)" = "Darwin" ] ; then
     echo "Not setup for Darwin, sorry!" 1>&2 && exit 1
 elif [ -f /etc/os-release ] && cat /etc/os-release | grep -qi Oracle  ; then
     PLATFORM="oracle"
-    VENDOR="$(cat /etc/os-release | grep -E ^VERSION= | awk -F= '{ print $2 }' | sed 's,",,g')"
+    VENDOR="ol$(cat /etc/os-release | grep -E ^VERSION= | awk -F= '{ print $2 }' | sed 's,",,g')"
 else
     echo "Failed to determine platform" 1>&2 && exit 1
 fi
@@ -25,18 +44,60 @@ archive_it()
 {
     local TOOL="$1"
     local ARCHIVE="${TOOL}--x86_64--${VENDOR}--linux.tar.xz"
+    cd "$TOOLCHAINS_DIR"
     tar -c "$TOOL" | xz -c9e > "${ARCHIVE}"
     sha256sum "$ARCHIVE" > "$ARCHIVE.sha256" 
 }
 
-THIS_DIR="$(cd $(dirname "$0") ; pwd)"
-LLVMS="llvm-16.0.6 llvm-15.0.7 llvm-14.0.6 llvm-13.0.1"
-GCCS="gcc-13.2.0 gcc-12.3.0 gcc-11.4.0 gcc-10.5.0 gcc-9.5.0"
-for TOOL in $LLVMS $GGCS ; do
+build_it()
+{
+    local TOOL="$1"
     cd "$THIS_DIR"
-    ./build-toolchain.sh --no-cleanup "$TOOL"
-    cd "$TOOLCHAINS_DIR"
-    wait
-    archive_it "$TOOL" &
-done     
+    ./build-toolchain.sh --no-cleanup "$TOOL"    
+}
+
+DO_ARCHIVE=false
+DO_BUILD=false
+TOOLCHAIN=""
+
+for ARG in "$@" ; do
+    [ "$ARG" = "-h" ] || [ "$ARG" = "--help" ] && show_help && exit 0
+done
+
+if (( $# == 0 )) ; then
+    echo "Type -h for help" 1>&2 && exit 1
+fi
+
+while (( $# > 0 )) ; do
+    ARG="$1"
+    shift
+    [ "$ARG" = "build" ] && DO_BUILD=true && continue
+    [ "$ARG" = "archive" ] && DO_ARCHIVE=true && continue
+    if [ "$TOOLCHAIN" != "" ] ; then
+        echo "unexpected argument: '$ARG', toolchain already set to '$TOOLCHAIN'" 1>&2
+        exit 1
+    fi
+    TOOLCHAIN="$ARG"
+done
+
+TOOLCHAINS="$LLVMS $GCCS"
+if [ "$TOOLCHAIN" != ":all" ] ; then
+    # Check toolchain is in the list
+    if ! echo "$TOOLCHAINS" | grep -q "$TOOLCHAIN" ; then
+        echo "Unexpected toolchain: '$TOOLCHAIN'" 1>&2
+        exit 1
+    fi
+    TOOLCHAINS="$TOOLCHAIN"
+fi
+
+if ! $DO_BUILD && ! $DO_ARCHIVE ; then
+    echo "Must specify one of 'archive' or 'build' to actually do anything!" 1>&2
+    exit 1
+fi
+
+for TOOL in $TOOLCHAINS ; do
+    $DO_BUILD && build_it "$TOOL" || true
+    $DO_ARCHIVE && archive_it "$TOOL" || true
+done
+
 
