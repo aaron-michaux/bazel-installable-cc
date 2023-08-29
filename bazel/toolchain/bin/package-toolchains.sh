e5ec7678c24e528801c27d664637e31e8005b922
Aaron Michaux <aaron@pageofswords.net>
2023-08-29 14:01:20 -0400

    xz in parallel

--- bazel/toolchain/bin/package-toolchains.sh
+++ bazel/toolchain/bin/package-toolchains.sh
@@ -21,6 +21,14 @@ else
     echo "Failed to determine platform" 1>&2 && exit 1
 fi
 
+archive_it()
+{
+    local TOOL="$1"
+    local ARCHIVE="${TOOL}--x86_64--${VENDOR}--linux.tar.xz"
+    tar -c "$TOOL" | xz -c9e > "${ARCHIVE}"
+    sha256sum "$ARCHIVE" > "$ARCHIVE.sha256" 
+}
+
 THIS_DIR="$(cd $(dirname "$0") ; pwd)"
 LLVMS="llvm-16.0.6 llvm-15.0.7 llvm-14.0.6 llvm-13.0.1"
 GCCS="gcc-13.2.0 gcc-12.3.0 gcc-11.4.0 gcc-10.5.0 gcc-9.5.0"
@@ -28,8 +36,7 @@ for TOOL in $LLVMS $GGCS ; do
     cd "$THIS_DIR"
     ./build-toolchain.sh "$TOOL"
     cd "$TOOLCHAINS_DIR"
-    ARCHIVE="${TOOL}--x86_64--${VENDOR}--linux.tar.xz"
-    tar -c "$TOOL" | xz -c9e > "${ARCHIVE}"
-    sha256sum "$ARCHIVE" > "$ARCHIVE.sha256"
+    wait
+    archive_it "$TOOL" &
 done     
 
